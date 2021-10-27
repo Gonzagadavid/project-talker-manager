528a732c337cb35c8a5f6e50b7a897e104f1799a
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-10-27 15:58:50 -0300

    implementado tratamento de erro na rota get /talker

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -3,9 +3,13 @@ const getTalkers = require('../functions/getTalkers');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  const talker = getTalkers();
-  res.status(200).json(talker);
+router.get('/', (req, res, next) => {
+  try {
+    const talker = getTalkers();
+    res.status(200).json(talker);
+  } catch (_err) {
+    next({ status: 500, message: 'ocorreu um erro com a requisição' });
+  }
 });
 
 module.exports = router;
